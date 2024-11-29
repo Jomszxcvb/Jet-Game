ee46bd34ac6fea63905632829d83d4b7c07a07e2
Jomszxc <202210892@fit.edu.ph>
2024-11-30 02:18:43 +0800

    Implement PowerUp class with methods and rendering
    Added PowerUp class implementation in PowerUp.cpp, including:
    - Constructor initializing type, position, and speed
    - update method to move power-up towards player
    - checkCollision method to detect collisions with player
    - getType method to return power-up type
    - getColor method to set color based on type
    - render method to draw power-up using OpenGL
    
    Updated PowerUp.h to define PowerUp class and methods:
    - Included necessary headers
    - Defined PowerUpType enum with HomingMissile and Gigantification

---
+++ Jet Game/PowerUp.cpp
@@ -0,0 +1,52 @@
+#include "PowerUp.h"
+#include <GL/glut.h>
+
+PowerUp::PowerUp(PowerUpType type, float x, float y) : type(type), posX(x), posY(y), speed(0.5f) {}
+
+void PowerUp::update(float deltaTime, float playerX, float playerY) {
+    // Move towards the player
+    float directionX = playerX - posX;
+    float directionY = playerY - posY;
+    float length = sqrt(directionX * directionX + directionY * directionY);
+    if (length != 0) {
+        directionX /= length;
+        directionY /= length;
+    }
+    posX += directionX * speed * deltaTime;
+    posY += directionY * speed * deltaTime;
+}
+
+bool PowerUp::checkCollision(float playerX, float playerY, float playerSize) const {
+    return (std::abs(posX - playerX) < playerSize && std::abs(posY - playerY) < playerSize);
+}
+
+PowerUpType PowerUp::getType() const {
+    return type;
+}
+
+void PowerUp::getColor(float& r, float& g, float& b) const {
+    switch (type) {
+    case PowerUpType::HomingMissile:
+        r = 1.0f; g = 0.0f; b = 0.0f; // Red color for HomingMissile
+        break;
+    case PowerUpType::Gigantification:
+        r = 0.0f; g = 0.0f; b = 1.0f; // Blue color for Gigantification
+        break;
+    }
+}
+
+void PowerUp::render() const {
+    float r, g, b;
+    getColor(r, g, b);
+
+    glPushMatrix();
+    glTranslatef(posX, posY, 0.0f);
+    glBegin(GL_QUADS);
+    glColor3f(r, g, b); // Set color based on power-up type
+    glVertex2f(-0.05f, -0.05f);
+    glVertex2f(0.05f, -0.05f);
+    glVertex2f(0.05f, 0.05f);
+    glVertex2f(-0.05f, 0.05f);
+    glEnd();
+    glPopMatrix();
+}
---
+++ Jet Game/PowerUp.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <GL/glew.h>
+#include <cmath>
+
+enum class PowerUpType {
+    HomingMissile,
+    Gigantification
+};
+
+class PowerUp {
+public:
+    PowerUp(PowerUpType type, float x, float y);
+
+    void update(float deltaTime, float playerX, float playerY);
+    bool checkCollision(float playerX, float playerY, float playerSize) const;
+    PowerUpType getType() const;
+    void render() const;
+
+private:
+    PowerUpType type;
+    float posX, posY;
+    float speed;
+
+    void getColor(float& r, float& g, float& b) const;
+};
