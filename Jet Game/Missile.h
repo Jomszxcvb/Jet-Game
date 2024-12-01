47e5223ea428ddde5f3da67d25f2301b6818b5a7
Jomszxc <202210892@fit.edu.ph>
2024-12-01 22:49:48 +0800

    ###

--- Jet Game/Missile.h
+++ Jet Game/Missile.h
@@ -5,27 +5,27 @@ class Missile {
 public:
     Missile(float x = 0.0f, float y = 0.0f, float size = 0.01f, float dirX = 0.0f, float dirY = 1.0f);
     ~Missile();
-
     float getPosX() const;
     float getPosY() const;
     float getSize() const;
-
     void render();
     void update(float speed);
-
     void scale(float scaleX, float scaleY);
-
     bool isOutOfBound() const;
-
     float getMissileSpeed() const;
     void setMissileSpeed(float speed);
 
 private:
+    void initVBO();
+    void renderOuterBullet() const;
+    void renderMiddleBullet() const;
+    void renderInnerBullet() const;
+
     float posX, posY;
     float size;
     float missileSpeed;
     bool outOfBound;
     float directionX, directionY;
     GLuint vbo;
-    void initVBO();
 };
+
